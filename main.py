16c9efb3f0277076735163bc31594fe41f8642b6
Tehsin Pathan <pathantehsin252@gmail.com>
2026-08-22 11:30:23 +0530

    demo

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Explainable Detection of AI-Generated Images and Videos Using Traditional ML")
+    print("Project initialized successfully.")
+
+if __name__ == "__main__":
+    main()
